fe800ba101e1ac87ca285be170917d5bb20810d0
jdmartens <jeremydmartens@gmail.com>
2025-03-12 18:44:37 -0500

    just running one test to debug test failure

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
-coverage run --source=app -m pytest
+# coverage run --source=app -m pytest
+coverage run --source=app -m pytest tests/scripts/test_backend_pre_start.py
 coverage report --show-missing
 coverage html --title "${@-coverage}"
